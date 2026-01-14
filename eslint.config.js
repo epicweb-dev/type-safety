91b6e3b36ab0f65f8d7b202cc26d2c5408282a53
Kent C. Dodds <me+github@kentcdodds.com>
2026-01-14 12:58:05 -0700

    fix: update ESLint config with TypeScript parser options

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 import defaultConfig from '@epic-web/config/eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import("eslint").Linter.Config} */
 export default [
 	...defaultConfig,
 	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: __dirname,
+				allowDefaultProject: true,
+			},
+		},
 		rules: {
 			// we leave unused vars around for the exercises
 			'no-unused-vars': 'off',
